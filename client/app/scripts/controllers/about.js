b969a75a4383a570c5cfbe0915e9cbec35347785
gurbano <g.urbano@gmail.com>
2015-10-07 15:25:17 +0200

    first

---
+++ client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pipboyClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pipboyClientApp
+ */
+angular.module('pipboyClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pipboyClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pipboyClientApp
+ */
+angular.module('pipboyClientApp')
+  .controller('MainCtrl', function () {
+  	
+  	
+
+  	
+  });
+
